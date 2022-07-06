597099749fb61d8769a78e3d286a4d8e0176e81a
veta41 <74262484+veta41@users.noreply.github.com>
2022-07-06 22:53:16 +0300

    fix

--- src/JS/fetchImagesService.js
+++ src/JS/fetchImagesService.js
@@ -1,38 +1,6 @@
-// import axios from "axios";
-// axios.defaults.baseURL = 'https://pixabay.com/api';
-// const API_KEY = '28265078-0316afef2c157389b1fbaa4c7';
-// const PARAM = 'per_page=40&orientation=horizontal&image_type=photo&safesearch=true';
-
-// export class FetchImagesService {
-//     constructor(){
-//         this.searchQuery = '';
-//         this.page = 1;
-//     }
-
-//     async fetchImages () {
-//         try{
-//             const responce = await axios.get(`/?key=${API_KEY}&q=${this.searchQuery}&page=${this.page}&${PARAM}`);
-//             this.incrementPage();
-//             return responce;
-//             console.log(responce);
-        
-//         } catch (error) {
-//             console.log(error);
-//         }
-        
-//     }
-
-//     incrementPage() {
-//         this.page += 1;
-//     }
-
-//     resetPage() {
-//         this.page = 1;
-//     }
-// }
 
 import axios from 'axios';
-//----------------------------------------------//
+
 
 const API_KEY = '28265078-0316afef2c157389b1fbaa4c7';
 const BASE_URL = 'https://pixabay.com/api/';
@@ -54,4 +22,4 @@ export async function fetchImages(name, page) {
   return await axios(params);
 }
 
-// export { getImgs };
+
